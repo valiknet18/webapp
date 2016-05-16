09b018455a7e1b93f67caceb22c38d9d9d44072d
valik-pc <valik.v1per@gmail.com>
2016-05-16 23:12:07 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+   mix.sass([
+     'app.scss',
+     //'controllers.scss'
+   ], 'public/assets/css');
 });
